eb6a78a7ba1b869ecb63b51ecd504c098cbca6fc
Sierra <35674159+SierraDev@users.noreply.github.com>
2018-08-13 23:30:29 -0400

    Add files via upload

---
+++ js/data/songs.js
@@ -0,0 +1,85 @@
+export const songsList = [{
+    "id": "1",
+    "url": "/music-player/music/Velius.mp3",
+    "time": "05:39",
+    "title": "Velius",
+    "artist": "Helios"
+  },
+  {
+    "id": "2",
+    "url": "/music-player/music/CoastOff.mp3",
+    "time": "04:53",
+    "title": "Coast Off",
+    "artist": "Helios"
+  },
+  {
+    "id": "3",
+    "url": "/music-player/music/A Mountain of Ice.mp3",
+    "time": "04:26",
+    "title": "A Mountain of Ice",
+    "artist": "Helios"
+  },
+  {
+    "id": "4",
+    "url": "/music-player/music/Isoline.mp3",
+    "time": "05:05",
+    "title": "Isoline",
+    "artist": "Helios"
+  },
+  {
+    "id": "5",
+    "url": "/music-player/music/BlessThisMorningYear.mp3",
+    "time": "06:03",
+    "title": "Bless This Morning Year",
+    "artist": "Helios"
+  },
+  {
+    "id": "6",
+    "url": "/music-player/music/Budapest Eskimos.mp3",
+    "time": "07:00",
+    "title": "Budapest Eskimos",
+    "artist": "The Lushlife Project"
+  },
+  {
+    "id": "7",
+    "url": "/music-player/music/ThemeofLaura.mp3",
+    "time": "03:25",
+    "title": "Theme of Laura",
+    "artist": "Akira Yamaoka"
+  },
+  {
+    "id": "8",
+    "url": "/music-player/music/Promise.mp3",
+    "time": "04:41",
+    "title": "Promise",
+    "artist": "Akira Yamaoka"
+  },
+  {
+    "id": "9",
+    "url": "/music-player/music/Blue Girl.mp3",
+    "time": "05:01",
+    "title": "Blue Girl",
+    "artist": "Chromatics"
+  },
+  {
+    "id": "10",
+    "url": "/music-player/music/Cherry.mp3",
+    "time": "04:44",
+    "title": "Cherry",
+    "artist": "Chromatics"
+  },
+  {
+    "id": "11",
+    "url": "/music-player/music/Shadow.mp3",
+    "time": "04:20",
+    "title": "Shadow",
+    "artist": "Chromatics"
+  },
+  {
+    "id": "12",
+    "url": "/music-player/music/CoeurCroisé.mp3",
+    "time": "03:40",
+    "title": "Coeur Croisé",
+    "artist": "Polo & Pan"
+  },
+];
